713eee5c76a9462d35cdf8b437b8926e823b4815
Pratyush1606 <jaiswalpratyush2015@gmail.com>
2022-05-11 15:42:16 +0530

    Added Book model and serializer
    Signed-off-by: Pratyush1606 <jaiswalpratyush2015@gmail.com>

--- books_app/models.py
+++ books_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    book_id = models.AutoField(primary_key=True, unique=True)
+    name = models.CharField(max_length=100)
+    published = models.IntegerField()
+    author = models.CharField(max_length=100)
+    domain = models.CharField(max_length=200)
+    is_available = models.BooleanField(default=False)
