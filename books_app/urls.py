2ae2220759980e6d9c76d54539b30154d486dff1
Pratyush1606 <jaiswalpratyush2015@gmail.com>
2022-05-11 11:15:05 +0530

    Added django app books_app
    Signed-off-by: Pratyush1606 <jaiswalpratyush2015@gmail.com>

--- books_app/urls.py
+++ books_app/urls.py
@@ -13,10 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
+from books_app import views
 
+app_name = "books_app"
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('books_app.urls', namespace='books_app')),
+    
 ]
